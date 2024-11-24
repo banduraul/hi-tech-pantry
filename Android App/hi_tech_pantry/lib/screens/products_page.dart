d3e82a43c6562faf82d7588893e68495bdf65fe6
Raul-Valentin Bandu <banduraul0@gmail.com>
2024-11-24 02:22:36 +0000

    Implemented product search functionality

--- Android App/hi_tech_pantry/lib/screens/products_page.dart
+++ Android App/hi_tech_pantry/lib/screens/products_page.dart
@@ -13,55 +13,132 @@ class ProductsPage extends StatefulWidget {
 }
 
 class _ProductsPageState extends State<ProductsPage> {
+  bool _isSearching = false;
+  String _searchQuery = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      appBar: AppBar(
-        title: const Text('Products'),
-      ),
       body: Consumer<ApplicationState>(
-        builder: (context, appState, _) => appState.productInfo.isEmpty
-        ? Center(child: Text('You have no products deposited'))
-        : Stack(
-          children: [
-            ListView.builder(
-              padding: EdgeInsets.only(top: appState.hasExpiredProducts ? 36.0 : 0.0),
-              itemCount: appState.productInfo.length,
-              itemBuilder: (context, index) {
-                final productInfo = appState.productInfo[index];
-                return Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 16.0),
-                  child: ProductCard(productInfo: productInfo),
-                );
-              }
-            ),
-
-            if (appState.hasExpiredProducts)
-              Positioned(
-                top: 5,
-                left: 16,
-                right: 16,
-                child: Container(
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    color: Colors.teal.shade400,
-                    borderRadius: BorderRadius.circular(16.0),
-                  ),
-                  alignment: Alignment.bottomCenter,
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    'Swipe left on the expired products to delete them',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold
+        builder: (context, appState, _) {
+          final filteredProducts = appState.productInfo.where((product) => product.name.toLowerCase().contains(_searchQuery)).toList();
+          final filteredProductsHasExpired = filteredProducts.any((product) => product.isExpired);
+          return appState.productInfo.isEmpty
+            ? Column(
+              children: [
+                AppBar(
+                  title: Text('Products'),
+                ),
+                Expanded(
+                  child: Center(
+                    child: Text('No products found')
+                  )
+                ),
+              ],
+            )
+            : Column(
+              children: [
+                Stack(
+                  children: [
+                    if (filteredProductsHasExpired)
+                      Material(
+                        elevation: 3,
+                        borderRadius: BorderRadius.circular(16.0),
+                        child: Container(
+                          width: double.infinity,
+                          height: 105,
+                          decoration: BoxDecoration(
+                            color: Colors.teal.shade400,
+                            borderRadius: BorderRadius.circular(16.0),
+                          ),
+                          alignment: Alignment.bottomCenter,
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            'Swipe left on the expired products to delete them',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold
+                            ),
+                          ),
+                        ),
+                      ),
+                    AppBar(
+                      title: Padding(
+                        padding: const EdgeInsets.only(left: 70),
+                        child: AnimatedSwitcher(
+                          duration: const Duration(milliseconds: 400),
+                          transitionBuilder: (child, animation) {
+                            return SizeTransition(
+                              sizeFactor: animation,
+                              axisAlignment: -1.0,
+                              child: child
+                            );
+                          },
+                          child: _isSearching
+                            ? TextField(
+                                key: ValueKey('searchField'),
+                                onChanged: (value) {
+                                  setState(() {
+                                    _searchQuery = value;
+                                  });
+                                },
+                                decoration: InputDecoration(
+                                  hintText: 'Search products...',
+                                  border: InputBorder.none,
+                                  hintStyle: TextStyle(color: Colors.grey.shade700),
+                                ),
+                                autofocus: true,
+                              )
+                            : Text(
+                                'Products',
+                                key: ValueKey('title'),
+                                style: TextStyle(color: Colors.blue.shade700)
+                              ),
+                        ),
+                      ),
+                      actions: [
+                        Padding(
+                          padding: const EdgeInsets.only(right: 10),
+                          child: IconButton(
+                            icon: Icon(
+                              _isSearching
+                                ? Icons.close
+                                : Icons.search,
+                              color: Colors.black,
+                              size: 27,
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                _isSearching = !_isSearching;
+                                if (!_isSearching) {
+                                  _searchQuery = '';
+                                }
+                              });
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Expanded(
+                    child: ListView.builder(
+                      padding: EdgeInsets.only(top: 0.0),
+                      itemCount: filteredProducts.length,
+                      itemBuilder: (context, index) {
+                        final productInfo = filteredProducts[index];
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 16.0),
+                          child: ProductCard(productInfo: productInfo),
+                        );
+                      }
                     ),
                   ),
-                ),
-              )
-          ]
-        )
+              ],
+            );
+        }
       ),
     );
   }
